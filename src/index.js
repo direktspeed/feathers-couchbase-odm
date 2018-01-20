45c8509a36d5b24e1b90908a32cbaf11082e7d12
Frank Lemanschik <frank@dspeed.eu>
2018-01-20 12:15:43 +0000

    Feathers 3.x

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const uuid = require('uuid/v4');
 const debug = require('debug');
 const Proto = require('uberproto');
-const filter = require('feathers-query-filters');
-const errors = require('feathers-errors');
-const { sorter, matcher, select, _ } = require('feathers-commons');
-const CouchbaseService  = require('feathers-couchbase').Service;
+const errors = require('@feathersjs/errors');
 
-//TODO: Add cas support
+const CouchbaseService = require('feathers-couchbase').Service;
+
+// TODO: Add cas support
 class CouchbaseOdmService extends CouchbaseService {
   constructor (options) {
     if (!options.keySpace) {
@@ -18,23 +17,23 @@ class CouchbaseOdmService extends CouchbaseService {
   extend (obj) {
     return Proto.extend(obj, this);
   }
-  setup(app,path){
+  setup (app, path) {
     this.app = app;
     this.path = path;
-    super.setup(app,path);
+    super.setup(app, path);
   }
-  find(params) {
+  find (params) {
     debug('feathers-couchbase-odm::find::info')(params);
     if (!this.keySpace) {
-      return Promise.reject('no keySpace for this service?');
+      return Promise.reject(new errors.GeneralError('no keySpace for this service?'));
     }
 
     params.query._type = this.keySpace;
     debug('feathers-couchbase-odm::find::info')(params);
-    return this._find(params).then((data)=>{
-      data.filter((itm)=>{
+    return this._find(params).then((data) => {
+      data.filter((itm) => {
         if (itm.cbKey) {
-          if (itm.cbKey === itm._type+'|'+itm._id) {
+          if (itm.cbKey === itm._type + '|' + itm._id) {
             debug('feathers-couchbase-odm::find::info::NotFiltered')(itm);
             return itm;
           } else {
@@ -45,27 +44,26 @@ class CouchbaseOdmService extends CouchbaseService {
       return data;
     });
   }
-  get(id, params) {
-    if (Array.isArray(id)){
-      debug('feathers-couchbase-odm::get::multi')(id,params);
+  get (id, params) {
+    if (Array.isArray(id)) {
+      debug('feathers-couchbase-odm::get::multi')(id, params);
     } else {
-      debug('feathers-couchbase-odm::get::single')(id,params);
+      debug('feathers-couchbase-odm::get::single')(id, params);
     }
 
-    return super.get(this.keySpace+'|'+id);
-    //.then((res)=>res[0]);
-    //return this._query({ _id: id, _type: this.keySpace}).then((res)=>res[0]);
-
+    return super.get(this.keySpace + '|' + id);
+    // .then((res)=>res[0]);
+    // return this._query({ _id: id, _type: this.keySpace}).then((res)=>res[0]);
   }
-  create(data, params) {
+  create (data, params) {
     if (data._id) {
-      debug('feathers-couchbase-odm::create::warning')('DEPRECATED: Called with id set %s',data._id);
+      debug('feathers-couchbase-odm::create::warning')('DEPRECATED: Called with id set %s', data._id);
     }
     if (data._type) {
-      debug('feathers-couchbase-odm::create::warning')('DEPRECATED: Called with type set %s',data._type);
+      debug('feathers-couchbase-odm::create::warning')('DEPRECATED: Called with type set %s', data._type);
     }
 
-    return this._create(this._createKey(data),params);
+    return this._create(this._createKey(data), params);
 
     /*
     if (Array.isArray(data)) {
@@ -73,58 +71,58 @@ class CouchbaseOdmService extends CouchbaseService {
     }
     */
   }
-  //update is single patch is mult
-  update(id, data) {
-    debug('feathers-couchbase-odm::update')(id,this._getKey(id),data);
-    return super.update(this._getKey(id),data);
+  // update is single patch is mult
+  update (id, data) {
+    debug('feathers-couchbase-odm::update')(id, this._getKey(id), data);
+    return super.update(this._getKey(id), data);
   }
-  patch(id, data, params) {
-    return this._patch(this._getKey(id),params);
+  patch (id, data, params) {
+    return this._patch(this._getKey(id), params);
   }
-  remove(id, params) {
-    debug('feathers-couchbase-odm::remove')(id,this._getKey(id),params);
-    return this._remove(this._getKey(id),params)
-      .catch((e)=>{
+  remove (id, params) {
+    debug('feathers-couchbase-odm::remove')(id, this._getKey(id), params);
+    return this._remove(this._getKey(id), params)
+      .catch((e) => {
         if (e.code === 13) {
-          debug('feathers-couchbase-odm::remove::Warning::KeyNotFoundTryId')(id,this._getKey(id),params);
-          return this._remove(id,params);
+          debug('feathers-couchbase-odm::remove::Warning::KeyNotFoundTryId')(id, this._getKey(id), params);
+          return this._remove(id, params);
         }
         debug('feathers-couchbase-odm::remove::error')(e);
       });
   }
-  _queryOdm(filter) {
-    //var QUERY = 'SELECT *, META(val).id FROM default val WHERE META(val).id LIKE "'+query+'%" ';
+  _queryOdm (filter) {
+    // var QUERY = 'SELECT *, META(val).id FROM default val WHERE META(val).id LIKE "'+query+'%" ';
     delete filter._type;
 
-    //var FILTER_ARRAY = [' WHERE val._type LIKE "Artist"'];
-    var FILTER_ARRAY = [' WHERE META(val).id LIKE "'+this.keySpace+'%"'];
-    Object.keys(filter).map((key)=>FILTER_ARRAY.push('val.'+key+' LIKE "' + filter[key] +'"'));
+    // var FILTER_ARRAY = [' WHERE val._type LIKE "Artist"'];
+    var FILTER_ARRAY = [' WHERE META(val).id LIKE "' + this.keySpace + '%"'];
+    Object.keys(filter).map((key) => FILTER_ARRAY.push('val.' + key + ' LIKE "' + filter[key] + '"'));
     var QUERY = 'SELECT *, META(val).id FROM default val' + FILTER_ARRAY.join(' AND ');
     debug('feathers-couchbase-odm:_queryOdm')(QUERY);
     return 'DEPERECATED';
-    //return db.runQuery(QUERY).then((res)=>res.map((itm)=>itm.val));
+    // return db.runQuery(QUERY).then((res)=>res.map((itm)=>itm.val));
   }
-  _getKey(id){
-    var keySpaceId = this.keySpace+'|'+id;
+  _getKey (id) {
+    var keySpaceId = this.keySpace + '|' + id;
     debug('feathers-couchbase-odm:_getKey')(keySpaceId);
     return keySpaceId;
   }
-  _createKey(data,params) {
-    //TODO: if params use that key
+  _createKey (data, params) {
+    // TODO: if params use that key
     if (data._id === undefined) {
       data._id = uuid();
     }
     if (data._type === undefined) {
       data._type = this.keySpace;
     }
-    var cbKey = data._type+'|'+data._id;
-    debug('feathers-couchbase-odm:_createKey')(cbKey,params);
+    var cbKey = data._type + '|' + data._id;
+    debug('feathers-couchbase-odm:_createKey')(cbKey, params);
     data.cbKey = cbKey;
     return data;
   }
 }
-//const Couchbase = new couchbaseService();
-//module.exports = Couchbase;
+// const Couchbase = new couchbaseService();
+// module.exports = Couchbase;
 export default function init (options) {
   return new CouchbaseOdmService(options);
 }
